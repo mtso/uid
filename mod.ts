2c5d696ee064b7ca5303108c8955ac9d06f4ea78
Matthew <8432061+mtso@users.noreply.github.com>
2021-05-28 09:33:49 -0700

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -3,7 +3,7 @@ import { Buffer } from 'https://deno.land/std@0.97.0/node/buffer.ts'
 
 export async function randomUid(len: number = 128, enc: string = 'hex') {
   return new Promise((resolve, reject) => {
-    randomBytes(Math.ceil(number / 8), (err, buf: Buffer) => {
+    randomBytes(Math.ceil(len / 8), (err, buf: Buffer) => {
       if (err) return reject(err);
       resolve(buf.toString(enc));
     });
